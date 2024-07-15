d8e55ce33c7aa0a334d5b86a6cf059c6a52cb5d0
gurujegangit <gurujeganr@onwardpath.com>
2024-07-15 21:49:37 +0530

    Initial commit

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let msg = 'Welcome to my new page!!!!!!';
+
+  const news = 'Page is coming soon!!!'
+
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+
+</script>
+
+<h1>News Page</h1>
+<p>{news}{msg}</p>
+<p>{count}</p>
+<a href="/">Home</a>
+<button on:click={increment} aria-label="Increase the counter by one">
+  Hit Me
+</button>
