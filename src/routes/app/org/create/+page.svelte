1bd0e52c78689bd05b3b6b21c1b01e584b226f52
swathisreeni <89260922+swathisreeni@users.noreply.github.com>
2024-07-22 21:55:04 +0530

    changes in org page

--- src/routes/app/org/create/+page.svelte
+++ src/routes/app/org/create/+page.svelte
@@ -1,18 +1,16 @@
 <script>
 	import CenterElements from '$lib/components/container/CenterElements.svelte';
 
-    /** @type {import('./$types').PageData} */
-    export let data;
-</script>
+    </script>
 
 
 
 <CenterElements>
-    <div class="form-control w-1/2 border-solid border-2 border-sky-500 rounded">
-        <label class="label">
-          <span class="label-text">Create Org</span>
-        </label>
-        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-md" />
-        <button class="btn btn-accent max-w-md">Accent</button>
-      </div>
+  <form class="form-control w-1/2 rounded">
+    <label class="label mb-2">
+      <span class="label-text">Create Org</span>
+    </label>
+    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-md mb-4" />
+    <button class="btn btn-accent max-w-md">Accent</button>
+  </form>
 </CenterElements>
