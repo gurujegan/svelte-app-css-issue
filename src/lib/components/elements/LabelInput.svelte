2bee9c32208d914925c8efcd2546e2c37114e48a
VenkatakrishnanSambath <krishnanvenkat003@gmail.com>
2024-07-23 22:28:33 +0530

    Create account page changes

---
+++ src/lib/components/elements/LabelInput.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let value = '';
+</script>
+
+<label class="label mb-2 text-2xl font-medium">Pan Id</label>
+<input
+    type="text"
+    placeholder="Type here"
+    class="input input-bordered w-full mb-4"
+    bind:value={value} />
---
+++ src/routes/app/create-account/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import DrawerContentContainer from '$lib/components/container/DrawerContentContainer.svelte';
+    import LabelInput from '$lib/components/elements/LabelInput.svelte';
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    let formData = {};
+</script>
+
+<DrawerContentContainer>
+    <form class="form-control md:w-1/2 w-full rounded">
+        <LabelInput name='Pan Id' value='Hi'></LabelInput>
+        <LabelInput name='Customer Name' value='Hi'></LabelInput>
+        <LabelInput name='Bank Account No' value='Hi'></LabelInput>
+        <LabelInput name='Bank Name' value='Hi'></LabelInput>
+        <LabelInput name='E-Mail' value='Hi'></LabelInput>
+        <LabelInput name='Mobile No' value='{formData.mobNo}'></LabelInput>
+		<button class="btn btn-info" on:click={createOrg}>Create Organization</button>
+	</form>
+</DrawerContentContainer>
