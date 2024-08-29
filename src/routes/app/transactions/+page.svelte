66062ddc96b86af1a030439a4d9c280139666176
gurujegangit <gurujeganr@onwardpath.com>
2024-08-29 23:33:12 +0530

    yup validation in transactions page

--- src/routes/app/transactions/+page.svelte
+++ src/routes/app/transactions/+page.svelte
@@ -2,19 +2,42 @@
 	import DefaultBodyContainer from '$lib/components/container/DefaultBodyContainer.svelte';
 	import LabelInput from '$lib/components/elements/LabelInput.svelte';
 	import { writable } from 'svelte/store';
-	import CustomerStore, { customers } from '$lib/stores/CustomerStore'
+	import CustomerStore, { customers } from '$lib/stores/CustomerStore';
 	import CustomerAccountStore, { customerAccounts } from '$lib/stores/CustomerAccountStore.js';
 	import TransactionAccountStore from '$lib/stores/TransactionAccountStore';
 	import ListRecords from '$lib/components/elements/ListRecords.svelte';
+	import { createForm } from 'svelte-forms-lib';
+	import * as yup from 'yup';
 
 	let formElements = TransactionAccountStore.formElements;
-
-
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	const savedTxns = writable([]);
+	const formLib = createForm({
+		initialValues: {
+			date: '',
+			description: '',
+			type: '',
+			amount: '',
+			category: ''
+		},
+		validationSchema: yup.object().shape({
+			date: yup.string().required(),
+			description: yup.string().required(),
+			type: yup.string().required(),
+			amount: yup.number().required(),
+			category: yup.string().required(),
+		}),
+		onSubmit: (values) => {
+			alert(JSON.stringify(values));
+			TransactionAccountStore.addTransaction(values);
+		}
+	});
+
+	const { form, state, errors, handleChange, handleSubmit, handleReset } = formLib;
+
+	const savedTxns = TransactionAccountStore.transactions;
 
 	const selectedBankAccount = writable({});
 
@@ -22,217 +45,184 @@
 
 	// To hold selected customer & respective accounts
 	const selectedCustomer = writable({});
-	$selectedCustomer.accounts = []
-
-	let newItem = { description: '', amount: '', category: '' };
+	$selectedCustomer.accounts = [];
 
 	function deleteRow(index) {
 		$savedTxns.splice(index, 1);
 		$savedTxns = $savedTxns;
 	}
 
-	function addNewTxn() {
-		savedTxns.set([...$savedTxns, newItem]);
-		newItem = { description: '', amount: '', category: '' };
-	}
-
 	function accountOnChange() {
-
-		$selectedBankAccount = {}
-		selectedBankAccount.set(CustomerAccountStore.getCustomerAccountById(newItem.accountId));
+		$selectedBankAccount = {};
+		selectedBankAccount.set(CustomerAccountStore.getCustomerAccountById($form.bankAccId));
 	}
 
-	function customerOnChange(){
+	function customerOnChange() {
+		selectedCustomer.set(CustomerStore.getCustomerById($form.custId));
 
-		selectedCustomer.set(CustomerStore.getCustomerById(newItem.custId))
-
-		$selectedBankAccount = {}
-		$selectedCustomer.accounts = []
-		$selectedCustomer.accounts = CustomerAccountStore.getCustomerAccountsByCustId(newItem.custId)
+		$selectedBankAccount = {};
+		$selectedCustomer.accounts = [];
+		$selectedCustomer.accounts = CustomerAccountStore.getCustomerAccountsByCustId($form.custId);
 	}
-
-	let formData = {
-		isChecked: false
-	};
-
-
 </script>
 
-{JSON.stringify($savedTxns)}
-
 <div class="flex flex-col gap-2 h-full">
+	
 	<div class="row1 flex gap-4">
-			<div class="item1 flex flex-col gap-8 justify-center w-1/2">
-				<div class="flex justify-between items-baseline">
+		<div class="item1 flex flex-col gap-8 justify-center w-1/2">
+			<div class="flex justify-between items-baseline">
 				<label class="label text-sm font-medium" for="cust-name">Customer</label>
-				<select class="select select-bordered w-1/2"
-				bind:value={newItem.custId}
-				on:change={customerOnChange}>
+				<select
+					class="select select-bordered w-1/2"
+					bind:value={$form.custId}
+					on:change={customerOnChange}
+				>
 					<option disabled selected>Select Customer</option>
 					{#each $customers as customer}
-					<option value={customer.id}>{customer.customerName} - {customer.panId}</option>
+						<option value={customer.id}>{customer.customerName} - {customer.panId}</option>
 					{/each}
 				</select>
-				</div>
-
-				<div class="flex justify-between items-baseline">
-					<label class="label text-sm font-medium w-1/2" for="accNo">Bank Account Number</label>
-					<select
-						class="select select-bordered w-1/2"
-						bind:value={newItem.accountId}
-						on:change={accountOnChange}
-					>
-						<option selected>Select Account</option>
-						{#each $selectedCustomer.accounts as customerAccount}
-							<option value={customerAccount.id}
-								>{customerAccount.bankAccNo}-{customerAccount.bankName}</option
-							>
-						{/each}
-					</select>
-				</div>
 			</div>
-			<div class="item2 w-1/2">
-				<!-- customerName: {$customerAccounts.customerName}
+
+			<div class="flex justify-between items-baseline">
+				<label class="label text-sm font-medium w-1/2" for="accNo">Bank Account Number</label>
+				<select
+					class="select select-bordered w-1/2"
+					bind:value={$form.bankAccId}
+					on:change={accountOnChange}
+				>
+					<option selected>Select Account</option>
+					{#each $selectedCustomer.accounts as customerAccount}
+						<option value={customerAccount.id}
+							>{customerAccount.bankAccNo}-{customerAccount.bankName}</option
+						>
+					{/each}
+				</select>
+			</div>
+		</div>
+		<div class="item2 w-1/2">
+			<!-- customerName: {$customerAccounts.customerName}
 				bankAccNo: {$customerAccounts.bankAccNo}
 				bankName:{$customerAccounts.bankName}
 				panId:{$customerAccounts.panId} -->
-				<div class="card bg-lime-300 shadow-xl">
-
-					<div class="card-body">
-					  <h2 class="card-title">Customer Name: {$selectedCustomer.customerName}</h2>
-					  <p>Customer Account:{$selectedBankAccount?.bankAccNo}</p>
-					</div>
-				  </div>
+			<div class="card bg-lime-300 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Customer Name: {$selectedCustomer.customerName}</h2>
+					<p>Customer Account:{$selectedBankAccount?.bankAccNo}</p>
+				</div>
 			</div>
+		</div>
 	</div>
+
 
 	<div class="row2 flex flex-row gap-1 h-full">
 		<div class="relative shadow-md sm:rounded-lg w-9/12">
 			<ListRecords {formElements} store={savedTxns} />
-			<!-- <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-				<thead
-					class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-				>
-					<tr>
-						<th scope="col" class="p-4">
-							<div class="flex items-center">
-								<input
-									id="checkbox-all"
-									type="checkbox"
-									class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-								/>
-								<label for="checkbox-all" class="sr-only">checkbox</label>
-							</div>
-						</th>
-						<th scope="col" class="px-6 py-3"> #Id </th>
-						<th scope="col" class="px-6 py-3"> Description </th>
-						<th scope="col" class="px-6 py-3"> Amount </th>
-						<th scope="col" class="px-6 py-3"> Category </th>
-						<th scope="col" class="px-6 py-3"> Action </th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each $savedTxns as txn, i}
-						<tr
-							class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-						>
-							<td class="w-4 p-4">
-								<div class="flex items-center">
-									<input
-										id="checkbox-table-1"
-										type="checkbox"
-										class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-									/>
-									<label for="checkbox-table-1" class="sr-only">checkbox</label>
-								</div>
-							</td>
-							<td class="px-6 py-4">
-								{txn.id}
-							</td>
-							<td class="px-6 py-4">
-								{txn.description}
-								Already Commented <input
-                                        type="text"
-                                        placeholder="Type here"
-                                        class="input input-bordered mb-4"
-                                        bind:value={txn.description}
-                                    />
-							</td>
-							<td class="px-6 py-4">
-								{txn.amount}
-							</td>
-							<td class="px-6 py-4">
-								{txn.category}
-							</td>
-							<td class="px-6 py-4">
-								<a
-									href="#/"
-									class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-									on:click={() => addRow(txn)}
-									>Save
-								</a>
-								<a
-									href="#/"
-									class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-									on:click={() => deleteRow(i)}
-									>Delete
-								</a>
-							</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table> -->
 		</div>
 
 		<div class="w-3/12 flex flex-col items-center">
 			<h1 class="text-2xl">Add New Transaction</h1>
 			<br />
-			<form class="form-control gap-4">
+			<form class="form-control gap-4" on:submit={handleSubmit}>
 				<div class="">
 					<input
 						type="date"
 						class="input input-bordered focus:bg-orange-100 focus:border-solid focus:border-2 focus:border-sky-500 w-full"
 						id="birthday"
-						name="Date"
-						bind:value={newItem.date}
+						name="date"
+						bind:value={$form.date}
+						on:click={handleChange}
+						on:blur={handleChange}
 					/>
-				</div>
-				{#each Object.entries(newItem) as [key, value, index]}
-					{#if key == 'amount' }
-						<div class="flex flex-row gap-2 justify-around">
-							<div class="flex gap-2">
-								<input type="radio" id="debit" value="debit" name="typeOfEntry" class="radio radio-secondary" checked="checked"
-								bind:group={newItem.type}>
-								<label for="debit1">Debit</label>
-							</div>
-
-							<div class="flex gap-2">
-								<input type="radio" id="credit" value="credit" name="typeOfEntry" class="radio radio-secondary mb-2"
-								bind:group={newItem.type}>
-								<label for="credit1">Credit</label>
-							</div>
-						</div>
+					{#if $errors.date}
+						<small>{$errors.date}</small>
 					{/if}
-					<div>
-						<!-- <label class="label mb-2 text-sm font-medium" for={key}
-							>{key[0].toUpperCase() + key.substr(1)}</label
-						> -->
+				</div>
+
+				<div class="">
+					<input
+						name="description"
+						type="text"
+						placeholder="Txn desc"
+						class="input input-bordered focus:bg-orange-100 focus:border-solid focus:border-2 focus:border-sky-500"
+						bind:value={$form.description}
+						on:click={handleChange}
+						on:blur={handleChange}
+					/>
+
+					{#if $errors.description}
+						<small>{$errors.description}</small>
+					{/if}
+				</div>
+
+				<div class="flex flex-row gap-2 justify-around">
+					<div class="flex gap-2">
 						<input
-							name={key}
-							type="text"
-							placeholder={key[0].toUpperCase() + key.substr(1)}
-							class="input input-bordered focus:bg-orange-100 focus:border-solid focus:border-2 focus:border-sky-500"
-							bind:value={newItem[key]}
+							type="radio"
+							id="debit"
+							value="debit"
+							name="type"
+							class="radio radio-secondary"
+							checked="checked"
+							bind:group={$form.type}
+							on:click={handleChange}
+							on:blur={handleChange}
 						/>
+						<label for="debit1">Debit</label>
 					</div>
-				{/each}
+
+					<div class="flex gap-2">
+						<input
+							type="radio"
+							id="credit"
+							value="credit"
+							name="type"
+							class="radio radio-secondary mb-2"
+							bind:group={$form.type}
+							on:click={handleChange}
+							on:blur={handleChange}
+						/>
+						<label for="credit1">Credit</label>
+					</div>
+					{#if $errors.type}
+						<small>{$errors.type}</small>
+					{/if}
+				</div>
+
+				<div>
+					<input
+						name="amount"
+						type="text"
+						placeholder="Enter amount"
+						class="input input-bordered focus:bg-orange-100 focus:border-solid focus:border-2 focus:border-sky-500"
+						bind:value={$form.amount}
+						on:click={handleChange}
+						on:blur={handleChange}
+					/>
+					{#if $errors.amount}
+						<small>{$errors.amount}</small>
+					{/if}
+				</div>
+
+
+				<div>
+					<input
+						name="category"
+						type="text"
+						placeholder="Category of txn"
+						class="input input-bordered focus:bg-orange-100 focus:border-solid focus:border-2 focus:border-sky-500"
+						bind:value={$form.category}
+						on:click={handleChange}
+						on:blur={handleChange}
+					/>
+					{#if $errors.category}
+						<small>{$errors.category}</small>
+					{/if}
+				</div>
+
 				<div class="flex flex-row justify-around w-full gap-2">
 					<button
-						class="btn btn-info transition ease-in-out delay-150
-		hover:-translate-y-1 hover:scale-110
-		hover:bg-indigo-500 hover:text-white
-		 duration-300 h-8 rounded-md w-1/2"
-						on:click={addNewTxn}
+						class="btn btn-info rounded-md w-1/2"
 					>
 						Add
 					</button>
@@ -242,7 +232,7 @@
 		hover:-translate-y-1 hover:scale-110
 		hover:bg-indigo-500 hover:text-white
 		 duration-300 h-8 rounded-md w-1/2"
-						on:click={null}
+						on:click={handleReset}
 					>
 						Reset
 					</button>
